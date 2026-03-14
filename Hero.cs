6c5ea99428128f56daddea97412171f97f0bd3e3
Aleksander Kostka <s29769@pjwstk.edu.pl>
2026-03-14 13:24:07 +0100

    Stworzenie klasy Hero

---
+++ Hero.cs
@@ -0,0 +1,13 @@
+﻿namespace apbdrepo;
+
+public class Hero
+{
+    private string name;
+    private int age;
+    
+    public Hero(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+}
