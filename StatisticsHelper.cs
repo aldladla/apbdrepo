8287c5b03e53770896a583f53dc3a0a4445f1a18
Aleksander Kostka <s29769@pjwstk.edu.pl>
2026-03-14 14:46:31 +0100

    dodanie dalszej logiki

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -32,6 +32,15 @@ public class StatisticsHelper
 
     public static int CalculateMin(int[] values)
     {
+        int min = values[0];
+        foreach (int v in values)
+        {
+            if (v < min)
+            {
+                min = v;
+            }
+        }
+        
         return 0;
     }
     
