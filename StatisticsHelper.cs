31004d20a89a7d05e0b9e8fafe04c35b986dcfa3
Aleksander Kostka <s29769@pjwstk.edu.pl>
2026-03-14 13:49:31 +0100

    Dodanie klasy StatisticsHelper

---
+++ StatisticsHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace apbdrepo;
+
+public class StatisticsHelper
+{
+    public static double CalculateAverage(int[] values)
+    {
+        int sum = 0;
+
+        foreach (int v in values)
+        {
+            sum += v;
+        }
+
+        return (double)sum / values.Length;
+    }
+}
